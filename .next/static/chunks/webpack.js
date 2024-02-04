47acbd857465757a5fbd2c217993225b45bfda9a
Hugo Ricardo <ricardodev08@gmail.com>
2024-02-04 16:48:01 -0300

    feat: add booking page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9619ca5d2082e3a4"; }
+/******/ 		__webpack_require__.h = function() { return "080e3c00342e1129"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -1,30 +1,54 @@
 
 
+import { Booking, Prisma } from "@prisma/client";
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { Card, CardContent } from "./ui/card";
+import { format, isPast } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
 
-const BookingItem = () => {
+
+interface BookingItemProps {
+    booking: Prisma.BookingGetPayload<{
+        include: {
+            service: true;
+            barbershop: true;
+        }
+    }>
+}
+
+
+const BookingItem = ({booking}: BookingItemProps) => {
     return ( 
         <Card>
             <CardContent className="p-5 flex justify-between" >
-                <div className="flex flex-col gap-2" >
-                    <Badge className="bg-[#221c3d] text-primary w-fit hover:bg-[#221c3d]" >Confirmado</Badge>
-                    <h2 className="font-bold" >Corte de cabelo</h2>
-                    <div className="flex items-center gap-2" >
-                        <Avatar className="h-6 w-6" >
-                            <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" alt="avatar bardearia"/>
-                            <AvatarFallback>A</AvatarFallback>
-                        </Avatar>
+        {!booking ? (
+            <h2>Não há agendamentos</h2>
+        ) : (
+            <div className="flex flex-col gap-2" >
+            <Badge variant={
+                isPast(booking.date) ? "secondary" : "default"
+            } className="w-fit" >
+                {isPast(booking.date) ? "Finalizado" : "Confirmado"}
+            </Badge>
+            <h2 className="font-bold" >{booking.service.name}</h2>
+            <div className="flex items-center gap-2" >
+                <Avatar className="h-6 w-6" >
+                    <AvatarImage src={booking.barbershop.imageUrl} alt="avatar bardearia"/>
+                    <AvatarFallback>A</AvatarFallback>
+                </Avatar>
 
-                        <h3 className="text-sm" >Vintage Barber</h3>
-                    </div>
-                </div>
+                <h3 className="text-sm" >{booking.barbershop.name}</h3>
+            </div>
+        </div>
+        )}
 
                 <div className="flex flex-col items-center justify-center border-l border-solid border-secondary" >
-                    <p className="text-sm" >Fevereiro</p>
-                    <p className="text-2xl" >06</p>
-                    <p className="text-sm" >09:45</p>
+                    <p className="text-sm capitalize" >{format(booking.date, "MMMM", {
+                        locale: ptBR
+                    })}</p>
+                    <p className="text-2xl" >{format(booking.date, "dd")}</p>
+                    <p className="text-sm" >{format(booking.date, "hh:mm")}</p>
                 </div>
             </CardContent>
         </Card>
